3cc149342f284dc9c7fb880f4ad981e598597f71
Fernando Costa Rodrigues <fernandocr@id.uff.br>
2025-05-01 11:51:25 -0300

    webmvc anottation to cors interceptor

--- src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
@@ -3,9 +3,11 @@ package com.example.loginauthapi.infra.cors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
+@EnableWebMvc
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
