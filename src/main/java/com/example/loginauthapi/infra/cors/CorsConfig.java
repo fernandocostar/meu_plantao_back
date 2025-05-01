b081dbe1d8f7466041cd145a69e8bf54960aa716
Fernando Costa Rodrigues <fernandocr@id.uff.br>
2025-05-01 01:26:41 -0300

    cors added http version of urls

--- src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
@@ -1,16 +1,19 @@
 package com.example.loginauthapi.infra.cors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("Cors configurations applied.");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://meuplantao.online", "https://www.meuplantao.online")
+                .allowedOrigins("http://localhost:3000", "https://meuplantao.online", "https://www.meuplantao.online", "http://meuplantao.online", "http://www.meuplantao.online")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
