aa2f8379181fbc719441615530af261023da200a
Fernando Costa Rodrigues <fernandocr@id.uff.br>
2025-05-01 13:31:24 -0300

    allow credentials

--- src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/loginauthapi/infra/cors/CorsConfig.java
@@ -17,10 +17,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000", "https://meuplantao.online", "https://www.meuplantao.online", "http://meuplantao.online", "http://www.meuplantao.online")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(false);
+                        .allowCredentials(true);
             }
         };
     }
