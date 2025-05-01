2294191c6c9b614ce648bb48c38bfec05906af0d
Fernando Costa Rodrigues <fernandocr@id.uff.br>
2025-05-01 13:27:56 -0300

    cors config by controller

--- src/main/java/com/example/loginauthapi/controllers/AccountController.java
+++ src/main/java/com/example/loginauthapi/controllers/AccountController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
--- src/main/java/com/example/loginauthapi/controllers/AuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
